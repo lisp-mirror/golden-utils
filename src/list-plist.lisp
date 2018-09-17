651105293fe98e339520d264cfd47764a66de3e8
Michael Fiano <mail@michaelfiano.com>
2018-09-17 16:35:34 -0400

    Added checks for some plist functions.
    The LOOP ON keyword will silently not do anything when given an atom, so
    this detects some user bugs.

--- src/list-plist.lisp
+++ src/list-plist.lisp
@@ -13,34 +13,44 @@
 
 (defun plist-remove (plist &rest keys)
   "Remove all `KEYS` and their associated values from `PLIST`. Non-destructive."
-  (loop :for (key value) :on plist :by #'cddr
-        :unless (member key keys :test #'eq)
-          :append (list key value)))
+  (if (plist-p plist)
+      (loop :for (key value) :on plist :by #'cddr
+            :unless (member key keys :test #'eq)
+              :append (list key value))
+      (error "~a is not a property list." plist)))
 
 (define-modify-macro plist-removef (&rest keys) plist-remove
   "Place-modifying macro for PLIST-REMOVE.")
 
 (defun plist-keys (plist)
   "Get a list of all keys in `PLIST`."
-  (loop :for (key value) :on plist :by #'cddr
-        :collect key))
+  (if (plist-p plist)
+      (loop :for (key value) :on plist :by #'cddr
+            :collect key)
+      (error "~a is not a property list." plist)))
 
 (defun plist-values (plist)
   "Get a list of all values in `PLIST`."
-  (loop :for (key value) :on plist :by #'cddr
-        :collect value))
+  (if (plist-p plist)
+      (loop :for (key value) :on plist :by #'cddr
+            :collect value)
+      (error "~a is not a property list." plist)))
 
 (defun plist->alist (plist)
   "Convert `PLIST` to an association list."
-  (loop :for (key value) :on plist :by #'cddr
-        :collect (cons key value)))
+  (if (plist-p plist)
+      (loop :for (key value) :on plist :by #'cddr
+            :collect (cons key value))
+      (error "~a is not a property list." plist)))
 
 (defun plist->hash (plist &rest args)
   "Convert `PLIST` to a hash table."
-  (let ((table (apply #'make-hash-table args)))
-    (loop :for (key value) :on plist :by #'cddr
-          :do (setf (gethash key table) value))
-    table))
+  (if (plist-p plist)
+      (let ((table (apply #'make-hash-table args)))
+        (loop :for (key value) :on plist :by #'cddr
+              :do (setf (gethash key table) value))
+        table)
+      (error "~a is not a property list." plist)))
 
 (defun plist-p (item)
   "Check whether or not `ITEM` is a property list."
