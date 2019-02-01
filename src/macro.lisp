a9b79f704d02c2d3a6bb7057b289e946e2291c70
Michael Fiano <mail@michaelfiano.com>
2019-02-01 09:02:01 -0500

    Minor fix

--- src/macro.lisp
+++ src/macro.lisp
@@ -19,6 +19,7 @@
 was successful, such as with GETHASH."
   (with-unique-names (found)
     `(multiple-value-bind (,var ,found) ,lookup
+       (declare (ignorable ,var))
        (when ,found
          ,@body))))
 
@@ -28,6 +29,7 @@ was successful, such as with GETHASH."
 was successful, such as with GETHASH."
   (with-unique-names (found)
     `(multiple-value-bind (,var ,found) ,lookup
+       (declare (ignorable ,var))
        (unless ,found
          ,@body))))
 
